02e447a6578b101c7912fdb828db6888b5936835
Allen Porter <allen.porter@gmail.com>
2021-04-22 23:18:15 -0700

    Update greetings to include multiple people
    Following tutorial in https://golang.org/doc/tutorial/greetings-multiple-people

--- hello/hello.go
+++ hello/hello.go
@@ -13,13 +13,15 @@ func main() {
   log.SetPrefix("greetings: ")
   log.SetFlags(0)
 
+  names := []string{"Gladys", "Samantha", "Darrin"}
+
   // Request a greeting message.
-  message, err := greetings.Hello("Gladys")
+  messages, err := greetings.Hellos(names)
   // If an error was returned, print it to the console and exit the program
   if err != nil {
     log.Fatal(err)
   }
 
   // If no error was returned, print the returned message to the console.
-  fmt.Println(message)
+  fmt.Println(messages)
 }
